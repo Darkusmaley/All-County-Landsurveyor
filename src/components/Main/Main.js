a1215e42ac70e8ac58daf5bc768c5017b90f76d2
Darkusmaley <daleymarcus091@gmail.com>
2024-10-24 17:02:47 -0400

    begun to add mobileview for main and header

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,35 @@
 import "./Main.css";
-import React from "react";
-import { Link } from "react-router-dom";
 import Header from "../Header/Header";
 
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 function Main() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 767);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <main className="main app__section">
       <Header />
-      <>
-        <section>
+      {isMobile ? (
+        <>
+          {/* <MobileView
+            currentRoute={"main"}
+            handleMobileModal={handleMobileModal}
+          /> */}
           <h1 className="main__title">
-            Looking for a professional land surveyor?
+            Looking for a professional land surveyor?{" "}
           </h1>
-          <h2 className="main__subtext">
+          <p className="main__subtext">
             Are you in need of a NY land surveyor for all of your land surveying
             needs? If so, you’ve come to the right place! Here at All County
             Land Surveying, we have the tools and experience to complete any and
@@ -21,62 +38,82 @@ function Main() {
             far past the boundaries of the 5 boroughs. With a new Western NY
             Branch servicing areas such as Rochester Buffalo, and Syracuse NY,
             we have you
-          </h2>
-          <span>
-            <Link to="/about">
-              <button className="main__subtext-link">More about us</button>
-            </Link>
-          </span>
-        </section>
+          </p>
+        </>
+      ) : (
+        <>
+          <section>
+            <h1 className="main__title">
+              Looking for a professional land surveyor?
+            </h1>
+            <h2 className="main__subtext">
+              Are you in need of a NY land surveyor for all of your land
+              surveying needs? If so, you’ve come to the right place! Here at
+              All County Land Surveying, we have the tools and experience to
+              complete any and all jobs you have. With more than 40 years of
+              combined land surveying experience, no job is too challenging. Our
+              services extend far past the boundaries of the 5 boroughs. With a
+              new Western NY Branch servicing areas such as Rochester Buffalo,
+              and Syracuse NY, we have you
+            </h2>
+            <span>
+              <Link to="/about">
+                <button className="main__subtext-link">More about us</button>
+              </Link>
+            </span>
+          </section>
 
-        <section className="surveying-services">
-          <h2 className="surveying-services__title">Our Surveying Services</h2>
-          <ul className="survey-services__list">
-            <li className="survey-services__list-service">
-              ALTA/NSPS Title Survey
-              <span className="survey-services__list-service_desc">
-                An ALTA/NSPS Land Title Survey is a boundary survey that
-                complies with the standards of the American Land Title
-                Association (ALTA), and the National Society of Professional
-                Surveyors, Inc.
-                <Link to="/services">
-                  <button className="surver-services__list-button_read-more">
-                    Read more...
-                  </button>
-                </Link>
-              </span>
-            </li>
-            <li className="survey-services__list-service">
-              Architectural Survey
-              <span className="survey-services__list-service_desc">
-                An Architectural Survey, also known as a Topographical Survey,
-                is a survey used by architects, engineers, contractors or
-                municipalities for design and construction purposes. It is a
-                boundary survey which may
-                <Link to="/services">
-                  <button className="surver-services__list-button_read-more">
-                    Read more...
-                  </button>
-                </Link>
-              </span>
-            </li>
-            <li className="survey-services__list-service">
-              Boundary/Title Survey
-              <span className="survey-services__list-service_desc">
-                A Boundary/Title Survey locates the boundaries of a property and
-                the extent of ownership. It will also identify property
-                encroachments, easements, and right-of-ways. A scaled map/plat
-                is prepared
-                <Link to="services">
-                  <button className="surver-services__list-button_read-more">
-                    Read more...
-                  </button>
-                </Link>
-              </span>
-            </li>
-          </ul>
-        </section>
-      </>
+          <section className="surveying-services">
+            <h2 className="surveying-services__title">
+              Our Surveying Services
+            </h2>
+            <ul className="survey-services__list">
+              <li className="survey-services__list-service">
+                ALTA/NSPS Title Survey
+                <span className="survey-services__list-service_desc">
+                  An ALTA/NSPS Land Title Survey is a boundary survey that
+                  complies with the standards of the American Land Title
+                  Association (ALTA), and the National Society of Professional
+                  Surveyors, Inc.
+                  <Link to="/services">
+                    <button className="surver-services__list-button_read-more">
+                      Read more...
+                    </button>
+                  </Link>
+                </span>
+              </li>
+              <li className="survey-services__list-service">
+                Architectural Survey
+                <span className="survey-services__list-service_desc">
+                  An Architectural Survey, also known as a Topographical Survey,
+                  is a survey used by architects, engineers, contractors or
+                  municipalities for design and construction purposes. It is a
+                  boundary survey which may
+                  <Link to="/services">
+                    <button className="surver-services__list-button_read-more">
+                      Read more...
+                    </button>
+                  </Link>
+                </span>
+              </li>
+              <li className="survey-services__list-service">
+                Boundary/Title Survey
+                <span className="survey-services__list-service_desc">
+                  A Boundary/Title Survey locates the boundaries of a property
+                  and the extent of ownership. It will also identify property
+                  encroachments, easements, and right-of-ways. A scaled map/plat
+                  is prepared
+                  <Link to="services">
+                    <button className="surver-services__list-button_read-more">
+                      Read more...
+                    </button>
+                  </Link>
+                </span>
+              </li>
+            </ul>
+          </section>
+        </>
+      )}
     </main>
   );
 }
